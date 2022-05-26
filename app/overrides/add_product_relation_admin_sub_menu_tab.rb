3f115201ac5e0ebeb02fefc86d5ef75995954d83
Didier Banlock <didier.banlock@gmail.com>
2022-05-26 12:40:20 +0200

    code cleanup

--- app/overrides/add_product_relation_admin_sub_menu_tab.rb
+++ app/overrides/add_product_relation_admin_sub_menu_tab.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/shared/sub_menu/_product',
-  name: 'add_product_relation_admin_sub_menu_tab',
-  insert_bottom: '[data-hook="admin_product_sub_tabs"]',
-  text: '<%= tab :relation_types, label: plural_resource_name(Spree::RelationType) %>'
+    virtual_path: 'spree/admin/shared/sub_menu/_product',
+    name: 'add_product_relation_admin_sub_menu_tab',
+    insert_bottom: '[data-hook="admin_product_sub_tabs"]',
+    original: '1c808e9a9760b1454e173bc5de4b264048406389',
+    text: '<%= tab :relation_types, label: plural_resource_name(Spree::RelationType) %>'
 )
